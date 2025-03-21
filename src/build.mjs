dac3f0980744e62d14765e5b8dd39a894dbc2462
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2025-03-20 23:37:49 -0700

    Use esbuild instead of tsc for unit tests (#631)

--- src/build.mjs
+++ src/build.mjs
@@ -1,19 +1,45 @@
 import { build } from 'esbuild';
+import minimist from 'minimist';
 
 /* eslint-disable-next-line no-undef */
-const isProd = process.argv.indexOf('--mode=production') >= 0;
-
-/* eslint-disable-next-line no-undef */
-console.log('Building AHK++ in', isProd ? 'production' : 'development', 'mode');
+const args = minimist(process.argv.slice(2));
+const isUnitTest = args.mode === 'unit-test';
+const isProd = args.mode === 'production';
+const entryPoints = [];
 
 // https://esbuild.github.io/api
-build({
-    entryPoints: ['./src/extension.ts'],
+const buildOptions = {
+    entryPoints: entryPoints,
     bundle: true,
-    outfile: 'dist/extension.js',
-    external: ['vscode'],
-    format: 'cjs',
+    format: 'cjs', // VS Code limitation, move to ESM requires investigation
     platform: 'node',
     minify: isProd,
-    sourcemap: !isProd,
-});
+    sourcemap: !isProd && !isUnitTest,
+};
+
+/* eslint-disable-next-line no-undef */
+console.log(`Building AHK++ in ${args.mode ?? 'development'} mode`);
+
+if (isUnitTest) {
+    const outdir = 'out';
+    entryPoints.push('./src/**/*utils.ts');
+    build({
+        ...buildOptions,
+        outdir: outdir,
+        bundle: true,
+        external: ['vscode'],
+    });
+    build({
+        ...buildOptions,
+        entryPoints: ['./src/**/*utils.test.ts'],
+        bundle: false,
+        outdir: outdir,
+    });
+} else {
+    entryPoints.push('./src/extension.ts');
+    build({
+        ...buildOptions,
+        outfile: 'dist/extension.js',
+        external: ['vscode'],
+    });
+}
